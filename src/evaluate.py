636bbf58f59ee36b9dc6c71f3dd64d86a5f01047
Walter Laurito <lauritowal@yahoo.com>
2023-12-01 02:14:00 +0100

    cleanup code

--- src/evaluate.py
+++ src/evaluate.py
@@ -12,16 +12,17 @@ load_dotenv()
 
 script_dir = Path(__file__).resolve().parent
 
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-log_dir = script_dir / f'../logs'
-log_dir.mkdir(parents=True, exist_ok=True)
-logging.basicConfig(filename=log_dir / f"experiment_{timestamp}.log", level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 NUM_EXAMPLES = 100
 ARTICULATION_PROMPT = "After that, can you articulate the pattern you have found and used for classification of the inputs in one short sentence? Separate the last sentence from the rest with \n\n"
+TASK = "lowercase" 
+
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+log_dir = script_dir / '../logs' / TASK
+log_dir.mkdir(parents=True, exist_ok=True)
+logging.basicConfig(filename=log_dir / f"experiment_{TASK}_{timestamp}.log", level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Load the JSON file containing prompt examples
-json_file_path =  script_dir / '../in_context_learning/lowercase.json'
+json_file_path =  script_dir / f'../in_context_learning/{TASK}.json'
 with open(json_file_path, 'r') as file:
     prompt_data = json.load(file)
 
@@ -31,7 +32,7 @@ formatted_prompt_examples = "\n".join(
 )
 
 # Load the dataset
-testset_path = script_dir / '../datasets/lowercase.csv'
+testset_path = script_dir / f'../datasets/{TASK}.csv'
 testset_df = pd.read_csv(testset_path)[:NUM_EXAMPLES]
 
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
@@ -46,59 +47,60 @@ for index, row in testset_df.iterrows():
     input_text = row['Input']
     testset += f"Input: \"{input_text}\" Label: \n"
 
-try:
-    # Prompt language model with instruction and testset
-    full_prompt = [
-        {"role": "system", "content": instruction + " " + ARTICULATION_PROMPT + testset},
-    ]
-    logging.info(f"full prompt: {pformat(full_prompt)}")
+# Prompt language model with instruction and testset
+full_prompt = [
+    {"role": "system", "content": instruction + " " + ARTICULATION_PROMPT + testset},
+]
+logging.info(f"full prompt: {pformat(full_prompt)}")
 
+try:
     # Make the API call
     response = client.chat.completions.create(
-        model="gpt-4", #-1106-preview", # use gpt4 for better results
+        model="gpt-4",
         messages=full_prompt
     )
-    content = response.choices[0].message.content
-
-    # Transform the string data into a format readable by pandas
-    # Replace 'Input: ' with empty string and ' Label: ' with a comma
-    content = content.replace('Input: "', '').replace('" Label: ', ',')
-    content = content.split('\n\n')
-    articulation = content[-1] # last sentence is the articulation
-    csv_data = "Input,Label\n" + content[0]
-
-    # Use StringIO to convert the string data to a file-like object
-    csv_file_like_object = io.StringIO(csv_data)
-
-    # Read the CSV data from the file-like object
-    csv_df = pd.read_csv(csv_file_like_object)
-
-    # write CSV file to disk
-    results_dir = script_dir / f'../results'
-    results_dir.mkdir(parents=True, exist_ok=True)
-    csv_df.to_csv(results_dir / f"experiment_{timestamp}.csv", index=False)
-
-    logging.info(f"content of response: {pformat(csv_df)}")
-
-    # Evaluate the results
-    # Initialize counter for correct classifications
-    correct = 0
-    for index, row in csv_df.iterrows():
-        input_text = row['Input']
-        predicted_label = row['Label']
-        ground_truth_label = testset_df.iloc[index]['Label']
-
-        # predicted_label = True if predicted_label == "True" else False
-        logging.info(f"Predicted label for {input_text}: {predicted_label}")
-        logging.info(f"Actual label for {input_text}: {ground_truth_label}")
-
-        correct += predicted_label == ground_truth_label
-
-    # Calculate accuracy
-    accuracy = correct / NUM_EXAMPLES
-    logging.info(f"Experiment completed. Accuracy: {accuracy}")
-    logging.info(f"Articulation {articulation}")
 except Exception as e:
     logging.warning(f"Failed full prompt: {pformat(full_prompt)}\n\n error: {pformat(str(e))}")
 
+content = response.choices[0].message.content
+breakpoint()
 
+# Transform the string data into a format readable by pandas
+# Replace 'Input: ' with empty string and ' Label: ' with a comma
+content = content.replace('Input: ', '').replace('Label: ', '')
+content = content.split('\n\n')
+articulation = content[-1] # last sentence is the articulation
+csv_data = "Input,Label\n" + content[0]
+
+# Use StringIO to convert the string data to a file-like object
+csv_file_like_object = io.StringIO(csv_data)
+
+# Read the CSV data from the file-like object
+csv_df = pd.read_csv(csv_file_like_object)
+
+# write CSV file to disk
+results_dir = script_dir / '../results' / TASK
+results_dir.mkdir(parents=True, exist_ok=True)
+csv_df.to_csv(results_dir / f"experiment_{TASK}_{timestamp}.csv", index=False)
+
+logging.info(f"content of response: {pformat(csv_df)}")
+
+# Evaluate the results
+# Initialize counter for correct classifications
+correct = 0
+for index, row in csv_df.iterrows():
+    input_text = row['Input']
+    predicted_label = row['Label']
+    ground_truth_label = testset_df.iloc[index]['Label']
+
+    logging.info(f"Correctly classified {input_text}? {predicted_label == ground_truth_label}")
+
+    if predicted_label == ground_truth_label:
+        correct += 1
+        logging.info(f"Num correctly classified: {correct}")
+
+# Calculate accuracy
+breakpoint()
+accuracy = correct / NUM_EXAMPLES
+logging.info(f"Experiment completed. Accuracy: {accuracy}")
+logging.info(f"Articulation {articulation}")
